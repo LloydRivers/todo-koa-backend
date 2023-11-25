dedef717a5b59704fd0230c0e129f7e7dd5c7813
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2023-11-25 20:41:57 +0000

    connected prisma to cockroach db

---
+++ prisma/migrations/20231125203710_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL,
+    "name" STRING NOT NULL,
+    "email" STRING NOT NULL,
+    "password" STRING NOT NULL,
+
+    CONSTRAINT "primary" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" UUID NOT NULL,
+    "text" STRING NOT NULL,
+    "isDone" BOOL NOT NULL,
+    "userId" UUID NOT NULL,
+
+    CONSTRAINT "primary" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "fk_user_id_ref_User" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
